4bb8dd8145849c8d7ac891f215cafc8bff15ec4c
Patricia Wong <patriciawong08@gmail.com>
2018-03-04 22:32:35 +0000

    automatically backed up by learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -1,1 +1,16 @@
 # Code your solution here!
+input = ""
+while input
+  print "Please enter a number between 1 and 5: "
+  user_response = gets.chomp.to_i
+  r = rand(1..5)
+  if user_response == r
+    puts "You guessed the correct number! The computer also guessed #{r}!"
+    break
+  elsif user_response == "exit"
+    puts "Goodbye"
+    break
+  else user_response != r
+    puts "The computer guessed #{r}."
+  end
+end
